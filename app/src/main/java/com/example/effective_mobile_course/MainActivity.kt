4c24e6e395485fdd3fb4afe036d4e3db35f6e802
Maksim Mishenko <sirmaksim47@gmail.com>
2022-11-26 17:07:59 +0600

    [+] add simple GUI

--- app/src/main/java/com/example/effective_mobile_course/MainActivity.kt
+++ app/src/main/java/com/example/effective_mobile_course/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.effective_mobile_course
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.effective_mobile_course.ui.theme.EffectivemobilecourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    mainScreen();
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun mainScreen() {
+    Surface(color = MaterialTheme.colors.error){
+        Column {
+            Image(
+                painter = painterResource(R.drawable.marvel),
+                contentDescription = "Contact profile picture"
+            )
+            Text(text = "Chose your hero")
+            LazyRow{
+                items(5) {
+                    card();
                 }
             }
         }
@@ -30,14 +59,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EffectivemobilecourseTheme {
-        Greeting("Android")
-    }
+fun card(){
+    Image(
+        painter = painterResource(R.drawable.marvel),
+        contentDescription = "Contact profile picture"
+    )
 }
